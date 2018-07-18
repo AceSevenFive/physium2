ae721f99a58488eae62c30886096d502aa653644
Ace SevenFive <acesevenfive@gmail.com>
2018-07-18 12:24:12 -0400

    TernaryValueCompare added, AddWithOverflow now uses shorts instead of ints

--- includes/mathlib.h
+++ includes/mathlib.h
@@ -1,6 +1,7 @@
 #ifndef MATHLIB
 #define MATHLIB
 
-int AddWithOverflow(int term1, int term2, int maxval);
+int AddWithOverflow(short term1, short term2, short maxval);
+int TernaryValueCompare(int term1, int term2);
 
 #endif
